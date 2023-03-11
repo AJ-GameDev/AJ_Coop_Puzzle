5a7b6e29806dd7e403ac0a0bd3bcac717ed3160a
Jorge Cobacho <108235595+Jhcob@users.noreply.github.com>
2023-03-11 19:19:28 +0100

    AJCP-1 Project commit (#1)

---
+++ Assets/StarterAssets/Mobile/Scripts/VirtualInputs/UIVirtualButton.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class UIVirtualButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerClickHandler
+{
+    [Header("Output")] public BoolEvent buttonStateOutputEvent;
+
+    public Event buttonClickOutputEvent;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        OutputButtonClickEvent();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        OutputButtonStateValue(true);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        OutputButtonStateValue(false);
+    }
+
+    void OutputButtonStateValue(bool buttonState)
+    {
+        buttonStateOutputEvent.Invoke(buttonState);
+    }
+
+    void OutputButtonClickEvent()
+    {
+        buttonClickOutputEvent.Invoke();
+    }
+
+    [System.Serializable]
+    public class BoolEvent : UnityEvent<bool>
+    {
+    }
+
+    [System.Serializable]
+    public class Event : UnityEvent
+    {
+    }
+}
