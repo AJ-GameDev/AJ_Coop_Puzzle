e0dc61b1ac75222e94b3fd2d42a928e5323724b6
Jorge Cobacho <jorgecobacho@gmail.com>
2023-03-11 18:56:47 +0100

    Project commit

---
+++ Assets/Pegasus/Scripts/PegasusBehaviour.cs
@@ -0,0 +1,16 @@
+#if UNITY_2017_1_OR_NEWER
+using System;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace Pegasus
+{
+    [Serializable]
+    public class PegasusBehaviour : PlayableBehaviour
+    {
+        [HideInInspector] public PegasusManager pegasusManager;
+
+        [HideInInspector] public float pegasusProgress;
+    }
+}
+#endif
